a8103af019510001340dfcac9fda5300575ca732
Mael17 <58207111+Mael17@users.noreply.github.com>
2020-05-05 23:33:20 +0100

    phase2

---
+++ helloworldmaven/src/main/java/frexia/helloworldmaven/App.java
@@ -0,0 +1,13 @@
+package frexia.helloworldmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
